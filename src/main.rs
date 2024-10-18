1a6fee0988e310f46751da8f0f1922416074a272
carson fenimore <carson@takecontrolinc.com>
2024-10-18 03:17:23 +0000

    validate callback output

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,49 @@
-use rslibcamlitelib::LibCamClient;
-use rslibcamlitelib::StreamParams;
-use rslibcamlitelib::StreamFormat;
-use rslibcamlitelib::ExternalCallback;
+use rslibcamlitelib::{LibCamClient,StreamParams, StreamFormat, ExternalCallback};
+
+use std::fs::File;
+use std::io::Write;
 
 mod timereporter;
 use timereporter::RateReporter;
+mod ppm;
 
+#[allow(non_snake_case, deprecated)]
 struct MyCallback {
+    lowresParams: StreamParams,
     h264Reporter: RateReporter,
     lowReporter: RateReporter,
 }
 
+#[allow(non_snake_case, deprecated)]
 impl MyCallback {
-    fn new() -> Self{
+    fn new(lowresParams: StreamParams) -> Self{
         MyCallback{
-            h264Reporter: RateReporter::new("h264"),
-            lowReporter: RateReporter::new("low")
+            lowresParams: lowresParams,
+            h264Reporter: RateReporter::new(1.0, "h264"),
+            lowReporter: RateReporter::new(1.0, "low")
         }
     }
 }
 
 #[allow(non_snake_case, deprecated)]
 impl ExternalCallback for MyCallback {
-    unsafe fn callbackH264(&mut self, _bytes: *mut u8, count: usize, timestamp_us: i64, keyframe: bool ){
+    unsafe fn callbackH264(&mut self, bytes: *mut u8, count: usize, _timestamp_us: i64, _keyframe: bool ){
         self.h264Reporter.tick();
+        let mut f = File::options()
+            .write(true)
+            .create(true)
+            .append(true).open("output.h264")
+            .unwrap();
+        let slice = unsafe { std::slice::from_raw_parts(bytes, count) };
+        f.write_all( slice ).expect("Failed to write/open h264 file");
     }
-    unsafe fn callbackLowres(&mut self, _bytes: *mut u8, count: usize){
+    unsafe fn callbackLowres(&mut self, bytes: *mut u8, count: usize){
+        if self.lowReporter.isTimeToReport() {
+            let slice = unsafe { std::slice::from_raw_parts(bytes, count) };
+            let outputFilename = "output.ppm";
+            ppm::writePPM( slice, self.lowresParams.width, self.lowresParams.height, outputFilename);
+            println!("Wrote {}", outputFilename);
+        }
         self.lowReporter.tick();
     }
 }
@@ -37,14 +55,15 @@ fn main() {
 
     // Setup streams
     println!("Setting up low res stream\n");
-    libcam.client.setupLowres(StreamParams{ width: 300, height: 300, format: StreamFormat::STREAM_FORMAT_RGB, framerate: 30});
+    let lowres = StreamParams{ width: 300, height: 300, format: StreamFormat::STREAM_FORMAT_RGB, framerate: 30};
+    libcam.client.setupLowres(&lowres);
 
     println!("Setting up high res stream\n");
     let h264Params = StreamParams{ width: 1920, height: 1080, format:  StreamFormat::STREAM_FORMAT_H264, framerate: 30};
     println!("Calling into setupH264\n");
-    libcam.client.setupH264(h264Params, 5, "main".to_owned(), "2mbps".to_owned());
+    libcam.client.setupH264(&h264Params, 5, &"main".to_owned(), &"2mbps".to_owned());
 
-    let mycb = Box::new(MyCallback::new());
+    let mycb = Box::new(MyCallback::new(lowres));
     libcam.setCallbacks(mycb);
     println!("Running...\n");
     libcam.run();
--- src/timereporter.rs
+++ src/timereporter.rs
@@ -1,27 +1,37 @@
 use std::time::Instant;
 
+#[allow(non_snake_case, deprecated)]
 pub struct RateReporter {
+    reportingSecs: f32,
     timeOfLastReport: Instant,
     count: u32, 
     tag: String,
 }
 
+#[allow(non_snake_case, deprecated)]
 impl RateReporter {
-    const REPORTING_SECS:f32 = 2.0;
-
-    pub fn new(tag: &str) -> Self{
+    pub fn new(reportingSecs:f32, tag: &str) -> Self{
         RateReporter {
+            reportingSecs: reportingSecs,
             timeOfLastReport: Instant::now(),
             count: 0,
             tag: tag.to_string()
         }
     }
+    pub fn duration(&self) -> f32 {
+        let dur = self.timeOfLastReport.elapsed();
+        let secs:f32 = dur.as_secs() as f32 + dur.subsec_nanos() as f32 / 1_000_000_000.0;
+        secs
+    }
+    pub fn isTimeToReport(&self) -> bool {
+        self.duration() >= self.reportingSecs
+    }
     pub fn tick(&mut self){
         self.count += 1;
-        let dur = self.timeOfLastReport.elapsed();
-        let deltaSecs: f32 = dur.as_secs() as f32 + dur.subsec_nanos() as f32 / 1_000_000_000.0;
-        if deltaSecs >= Self::REPORTING_SECS {
-            println!("{}: {} calls in past {} seconds", self.tag, self.count, deltaSecs);
+        if self.isTimeToReport() {
+            let dur = self.duration();
+            let rate: f32 = self.count as f32 / dur;
+            println!("{}: {} calls in past {} seconds; {} calls/sec", self.tag, self.count, dur, rate);
             self.count = 0;
             self.timeOfLastReport = Instant::now();
         }
